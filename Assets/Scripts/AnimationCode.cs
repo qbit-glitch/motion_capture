2cacd70d6226e3025940c4557d5994ec7a15aab4
Umesh Kumar Singh <brijbiharisingh1971@outlook.com>
2025-07-26 08:15:00 +0530

    3D Motion Capture using Webcam

--- Assets/Scripts/AnimationCode.cs
+++ Assets/Scripts/AnimationCode.cs
@@ -24,9 +24,9 @@ public class AnimationCode : MonoBehaviour
         for (int i = 0; i <= 32; i++)
         {
 
-            float x = float.Parse(points[0 + (i * 3)]) / 100;
-            float y = float.Parse(points[1 + (i * 3)]) / 100;
-            float z = float.Parse(points[2 + (i * 3)]) / 100;
+            float x = float.Parse(points[0 + (i * 3)]) / 200;
+            float y = float.Parse(points[1 + (i * 3)]) / 200;
+            float z = float.Parse(points[2 + (i * 3)]) / 500;
 
             Body[i].transform.localPosition = new Vector3(x, y, z);
         }
